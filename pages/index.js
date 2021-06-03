7e12b8d84a2c4b4e9e21107368d7e54c7e5e2a27
enotus.co <enotus@gmail.com>
2021-06-03 11:39:01 +0300

    test2

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello there! <a href="https://nextjs.org">Next.js!</a>
+          Привет <a href="https://nextjs.org">Руслан</a>
         </h1>
 
         <p className={styles.description}>
